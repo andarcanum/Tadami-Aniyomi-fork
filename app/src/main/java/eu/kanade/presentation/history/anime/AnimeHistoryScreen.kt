c2b0a193f17f02422c26de2cbe89bd7edf91e21b
andreykolesnikov <andreykolesnikov96@gmail.com>
2026-02-17 22:01:32 +0500

    Make Aurora history screens use transparent scaffold background

--- app/src/main/java/eu/kanade/presentation/history/anime/AnimeHistoryScreen.kt
+++ app/src/main/java/eu/kanade/presentation/history/anime/AnimeHistoryScreen.kt
@@ -3,12 +3,16 @@ package eu.kanade.presentation.history.anime
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import eu.kanade.domain.ui.UiPreferences
 import eu.kanade.presentation.components.relativeDateText
 import eu.kanade.presentation.history.anime.components.AnimeHistoryItem
 import eu.kanade.presentation.theme.TachiyomiPreviewTheme
@@ -22,6 +26,9 @@ import tachiyomi.presentation.core.components.ListGroupHeader
 import tachiyomi.presentation.core.components.material.Scaffold
 import tachiyomi.presentation.core.screens.EmptyScreen
 import tachiyomi.presentation.core.screens.LoadingScreen
+import tachiyomi.presentation.core.util.collectAsState
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 import java.time.LocalDate
 
 @Composable
@@ -34,7 +41,11 @@ fun AnimeHistoryScreen(
     onDialogChange: (AnimeHistoryScreenModel.Dialog?) -> Unit,
     searchQuery: String? = null,
 ) {
+    val uiPreferences = Injekt.get<UiPreferences>()
+    val theme by uiPreferences.appTheme().collectAsState()
+
     Scaffold(
+        containerColor = if (theme.isAuroraStyle) Color.Transparent else MaterialTheme.colorScheme.background,
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
     ) { contentPadding ->
         state.list.let {
--- app/src/main/java/eu/kanade/presentation/history/novel/NovelHistoryScreen.kt
+++ app/src/main/java/eu/kanade/presentation/history/novel/NovelHistoryScreen.kt
@@ -3,10 +3,14 @@ package eu.kanade.presentation.history.novel
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import eu.kanade.domain.ui.UiPreferences
 import eu.kanade.presentation.components.relativeDateText
 import eu.kanade.presentation.history.novel.components.NovelHistoryItem
 import eu.kanade.presentation.util.animateItemFastScroll
@@ -18,6 +22,9 @@ import tachiyomi.presentation.core.components.ListGroupHeader
 import tachiyomi.presentation.core.components.material.Scaffold
 import tachiyomi.presentation.core.screens.EmptyScreen
 import tachiyomi.presentation.core.screens.LoadingScreen
+import tachiyomi.presentation.core.util.collectAsState
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 import java.time.LocalDate
 
 @Composable
@@ -28,7 +35,11 @@ fun NovelHistoryScreen(
     onClickResume: (chapterId: Long) -> Unit,
     onDialogChange: (NovelHistoryScreenModel.Dialog?) -> Unit,
 ) {
+    val uiPreferences = Injekt.get<UiPreferences>()
+    val theme by uiPreferences.appTheme().collectAsState()
+
     Scaffold(
+        containerColor = if (theme.isAuroraStyle) Color.Transparent else MaterialTheme.colorScheme.background,
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
     ) { contentPadding ->
         state.list.let {
