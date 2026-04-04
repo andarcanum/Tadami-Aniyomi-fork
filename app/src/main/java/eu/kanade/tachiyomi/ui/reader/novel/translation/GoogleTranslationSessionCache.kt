4f9a6c9b661fcf43cfd2d6ba5ee0eb9b80721cad
andreykolesnikov <andreykolesnikov96@gmail.com>
2026-04-04 14:44:34 +0500

    [google-translate] add in-memory session cache

---
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/novel/translation/GoogleTranslationSessionCache.kt
@@ -0,0 +1,26 @@
+package eu.kanade.tachiyomi.ui.reader.novel.translation
+
+class GoogleTranslationSessionCache {
+
+    private val cache = LinkedHashMap<String, Map<Int, String>>()
+
+    fun buildKey(chapterId: Long, sourceLang: String, targetLang: String): String {
+        return "$chapterId|$sourceLang|$targetLang"
+    }
+
+    fun get(chapterId: Long, sourceLang: String, targetLang: String): Map<Int, String>? {
+        return cache[buildKey(chapterId, sourceLang, targetLang)]
+    }
+
+    fun put(chapterId: Long, sourceLang: String, targetLang: String, translatedByIndex: Map<Int, String>) {
+        cache[buildKey(chapterId, sourceLang, targetLang)] = translatedByIndex
+    }
+
+    fun remove(chapterId: Long, sourceLang: String, targetLang: String) {
+        cache.remove(buildKey(chapterId, sourceLang, targetLang))
+    }
+
+    fun clear() {
+        cache.clear()
+    }
+}
