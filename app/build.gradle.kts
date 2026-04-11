e75206c3ce8c8016c6235ac3396e2c887bfc67bd
andreykolesnikov <andreykolesnikov96@gmail.com>
2026-04-11 21:11:03 +0500

    [release] bump version to 0.36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     defaultConfig {
         applicationId = "com.tadami.aurora"
 
-        versionCode = 152
-        versionName = "0.35"
+        versionCode = 153
+        versionName = "0.36"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
