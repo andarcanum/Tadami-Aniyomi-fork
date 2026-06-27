34022cd4e986960672280fb0daa23db6a4e5bb33
andreykolesnikov <andreykolesnikov96@gmail.com>
2026-06-27 21:54:18 +0500

    chore: bump version to 0.54 (182)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     defaultConfig {
         applicationId = "com.tadami.aurora"
 
-        versionCode = 181
-        versionName = "0.53.7"
+        versionCode = 182
+        versionName = "0.54"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
