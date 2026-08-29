1cad7784170c005e4565accff1070bc4323af19a
andarcanum <jannople7@gmail.com>
2026-08-29 16:29:06 +0500

    [build] bump version to 0.61 (versionCode 204)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,9 +18,9 @@ android {
 
     defaultConfig {
         applicationId = "com.tadami.aurora"
-        versionCode = 203
+        versionCode = 204
 
-        versionName = "0.60.5"
+        versionName = "0.61"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
